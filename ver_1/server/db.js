f3fd9156855aaf767db9da24866e04c0f6d7f6a2
Giovanni Moncibaez <95672640+GioMonci@users.noreply.github.com>
2023-12-02 15:41:33 -0500

    Update db.js

--- ver_1/server/db.js
+++ ver_1/server/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     host: "localhost",
     user: "postgres",
-    password: "Tacopancho7",
+    password: "",
     port: 5432,
     database: "church"
 });
